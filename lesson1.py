eef2fbb93ba753f0d49c301b53933d4ff5fe404a
Nikabay <Nikabay>
2022-09-19 21:51:51 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+name  = "Nika"
+surname = "Khotcholava"
+age = 25
+height = 184.3
+jazi_da_wignebis_kitxva_misi_hobia = True
+uyvars_magari_mankanebi = False
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(height))
+print(type(jazi_da_wignebis_kitxva_misi_hobia))
+print(type(uyvars_magari_mankanebi))
+
+print(name + " " + surname + " " + "aris" + " " + str(age) + " " + "wlis da" + " " + str(height) + " " + "santimetris, romlis hobia jazzi da wignebis kitxva. mas ar uyavrs magari mankanebi")
+
+#amis martivad dawera asec sheilebhoda :P - print("Nika Khotcholava aris 25 wlis da 184.3 santimetris, romlis hobia jazzi da wignebis kitxva. maas ar uuyavrs magari mankanebi")
